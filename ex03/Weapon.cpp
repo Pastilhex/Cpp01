71c493cbcd41c10923cb213f268bc16306d8807f
Ivo Marques <110631286+Pastilhex@users.noreply.github.com>
2024-02-05 05:44:47 +0000

    Ready for tests

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -16,8 +16,8 @@ const std::string& Weapon::getType(){
 	return this->_type;
 }
 
-void Weapon::setType(std::string newType){
-	this->_type = newType;
+void Weapon::setType(std::string type){
+	this->_type = type;
 }
 
 Weapon::Weapon(std::string type){
--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -16,16 +16,13 @@
 #include <iostream>
 
 class Weapon {
-
 private:
 	std::string _type;
-
 public:
 	const std::string& getType();
 	void setType(std::string type);
 	Weapon(std::string type);
 	~Weapon();
-
 };
 
 #endif
