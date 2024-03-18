00e158bfff3f75b812b75d8d0d8063edfc08a651
Ivo Daniel Alves Marques <ialves-m@c2r2s11.42porto.com>
2024-03-18 15:56:20 +0000

    Final Update

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -12,14 +12,14 @@
 
 #include <iostream>
 
-int main(void) {
+int main(void)
+{
 	std::string var = "HI THIS IS BRAIN";
 	std::string* stringPTR = &var;
 	std::string& stringREF = var;
 
 	std::cout << &var << std::endl;
 	std::cout << stringPTR << std::endl;
-	//std::cout << &stringPTR << std::endl;
 	std::cout << &stringREF << std::endl;
 
 	std::cout << var << std::endl;
